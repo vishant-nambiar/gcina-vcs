53392742a197c8ecf726da8788aa8a4dfb41012b
Vishant Nambiar <vishantnambiar@gmail.com>
2021-08-23 00:37:49 +0530

    implemented functionality of creating .commit file and writing commit meta-data to it.

--- scripts.py
+++ scripts.py
@@ -1,3 +1,4 @@
+import sys
 from os import mkdir
 import subprocess
 from inspect import cleandoc
@@ -27,11 +28,22 @@ def commit():
     file_list = bash_execute('ls -a')
     if('.repo' not in file_list):
         raise Exception("Directory uninitialized.")
+    elif(len(sys.argv) < 3):
+        raise Exception("Please enter commit message.")
     
     #makes new commit directory with commit number as name
     file_list = bash_execute('ls .repo/snapshots/').split()
     num_of_files = len(file_list)
     new_file_number = num_of_files + 1
     bash_execute(f"mkdir .repo/snapshots/{new_file_number}")
-    #copies current working directory files into new commit directory
+    
+    #copies working directory files into new commit directory
     bash_execute(f'rsync -a --exclude=.repo --exclude=vcs --exclude=scripts.py ./ .repo/snapshots/{new_file_number}')
+    
+    #creates .commit file inside snapshot and stores info inside
+    user = bash_execute('whoami')
+    date_time = bash_execute("date")
+    commit_message = sys.argv[2]
+    commit_file = open(f".repo/snapshots/{new_file_number}/.commit", "w")
+    commit_file.write(user + date_time + commit_message + '\n')
+    commit_file.close()
