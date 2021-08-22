ca2c0eb7255b8bf8828fa9ed24be952d9c73cf34
Vishant Nambiar <vishantnambiar@gmail.com>
2021-08-22 09:52:02 +0530

    updated init error message

--- scripts.py
+++ scripts.py
@@ -1,6 +1,7 @@
 import subprocess
 from inspect import cleandoc
 
+#Executes any number of bash commands, one on each line
 def bash_execute(commands):
     commands = commands.split('\n')
     last_output = ''
@@ -12,7 +13,7 @@ def init():
     file_list = bash_execute("ls -a")
     
     if ".repo" in file_list:
-        raise Exception("Directory already initialized.")
+        raise Exception("Directory already initialized. If you think this is a mistake, please delete '.repo' before continuing.")
     
     command = cleandoc("""
         mkdir .repo
