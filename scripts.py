24277f5aed677b97669dfb56e739e64b1f16f0cc
Vishant Nambiar <vishantnambiar@gmail.com>
2021-09-05 15:47:24 +0530

    refactored commit and log to use hashes instead of numbers

--- scripts.py
+++ scripts.py
@@ -2,6 +2,7 @@ import sys
 from os import mkdir
 import subprocess
 from inspect import cleandoc
+import hashlib
 
 #Executes any number of bash commands, one on each line
 def bash_execute(commands):
@@ -32,36 +33,48 @@ def commit():
     elif(len(sys.argv) < 3):
         raise Exception("Please enter commit message.")
     
-    #makes new commit directory with commit number as name
-    file_list = bash_execute('ls .repo/snapshots/').split()
-    num_of_files = len(file_list)
-    new_file_number = num_of_files + 1
-    bash_execute(f"mkdir .repo/snapshots/{new_file_number}")
-    
-    #copies working directory files into new commit directory
-    bash_execute(f'rsync -a --exclude=.repo --exclude=vcs --exclude=scripts.py ./ .repo/snapshots/{new_file_number}')
-    
-    #creates .commit file inside snapshot and stores info inside
+    #makes new commit directory with commit hash as name
     user = bash_execute('whoami')
     date_time = bash_execute("date")
     commit_message = sys.argv[2]
-    commit_file = open(f".repo/snapshots/{new_file_number}/.commit", "w")
-    commit_file.write(user + date_time + commit_message + '\n')
-    commit_file.close()
+    hash = hashlib.sha1( bytes(user + date_time + commit_message, 'utf-8') ).hexdigest()
+    
+    bash_execute(f"mkdir .repo/snapshots/{hash}")
+    
+    #copies working directory files into new commit directory
+    bash_execute(f'rsync -a --exclude=.repo --exclude=vcs --exclude=scripts.py ./ .repo/snapshots/{hash}')
+    
+    #stores commit info in logs file
+    logs_file = open(f".repo/logs", "a")
+    logs_file.write('\n\n' + hash + '\n' + user + date_time + commit_message + '\n')
+    logs_file.close()
+    
 
 
 def log():
-    snapshot_file_list = bash_execute("ls .repo/snapshots/")
     log_output = ''
-    for file_number in snapshot_file_list.split('\n')[:-1]:
-        file = open(f".repo/snapshots/{file_number}/.commit", "r")
-        user = file.readline()[:-1]
-        date_time = file.readline()[:-1]
-        message = file.readline()[:-1]
-        file.close()
+    
+    file = open(f".repo/logs", "r")
+    file_data = file.read()
+    commits = file_data.split('\n\n')[1:]
+    file.close()
+    
+    for commit in commits:
+        #extracting commit info
+        commit = commit.split('\n')
+        commit = list( filter( lambda a: a != '', commit) )
+        
+        commit_hash = commit[0]
+        user = commit[1]
+        date_time = commit[2]
+        message = commit[3]
+        
         log_output += cleandoc(f"""
-            Snapshot: {file_number}
+            Snapshot: {commit_hash}
             Author: {user}
             Date & time: {date_time}
             Message: {message}""") + '\n\n'
+    
     print(log_output)
+        
+    
